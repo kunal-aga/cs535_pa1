f08540554fc1e6dd22493520767f25c436f1722a
Kunal Agarwal <65165791+iamkunalagarwal@users.noreply.github.com>
2023-02-12 12:43:37 -0700

    Update Citations1.scala

--- src/main/scala/Citations1.scala
+++ src/main/scala/Citations1.scala
@@ -1,30 +1,20 @@
 import org.apache.spark.sql.SparkSession
+import spark.implicits._
 
 object Citations1 {
 
     def main(args: Array[String]): Unit = {
-        
-        // val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-        // val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-        // val logData = spark.read.textFile(logFile).cache()
-        // val numAs = logData.filter(line => line.contains("a")).count()
-        // val numBs = logData.filter(line => line.contains("b")).count()
-        // println(s"Lines with a: $numAs, Lines with b: $numBs")
-        // spark.stop()
  
         val spark = SparkSession.builder.appName("Citations1").getOrCreate()
         val cit = spark.read.textFile("hdfs:///pa1/citations.txt")
+        cit.printSchema()
 
         val countOg = cit.count()
         println(s"Original lines count: $countOg")
 
-        // val filteredCit = cit.filter(!"value".contains("#"))
-        // val countFil = cit.count()
-        // println(s"Filtered lines count: $countFil")
-
-        cit.printSchema()
-
-        // val countFil = cit.filter(line => line.contains("#")).count()
+        val filteredCit = cit.filter($"value".contains("#"))
+        val countFil = filteredCit.count()
+        println(s"Filtered lines count: $countFil")
 
         spark.stop()
 
