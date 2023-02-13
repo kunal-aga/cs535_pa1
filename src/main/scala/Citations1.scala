ec8341e1b9c1835f582787457033b65139de92d2
Kunal Agarwal <65165791+iamkunalagarwal@users.noreply.github.com>
2023-02-13 12:30:06 -0700

    Update Citations1.scala

--- src/main/scala/Citations1.scala
+++ src/main/scala/Citations1.scala
@@ -87,8 +87,8 @@ object Citations1 {
 
         // Write output to HDFS
         val outputPath = "hdfs:///pa1/output_01/graph_density.txt"
-        graph_density_2.coalesce(1).saveAsTextFile(outputPath)
-
+        // graph_density_2.coalesce(1).saveAsTextFile(outputPath)
+        graph_density_2.coalesce(1).write.format("csv").save(outputPath)
         spark.stop()
     }
 }
