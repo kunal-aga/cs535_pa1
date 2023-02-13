33d43e7888aa435607163b04031fbf3e4eb0731c
Kunal Agarwal <65165791+iamkunalagarwal@users.noreply.github.com>
2023-02-13 12:12:46 -0700

    Update Citations1.scala

--- src/main/scala/Citations1.scala
+++ src/main/scala/Citations1.scala
@@ -10,13 +10,12 @@ object Citations1 {
         
         // Read Citations from HDFS
         var cit = spark.read.textFile("hdfs:///pa1/citations.txt")
-        cit.printSchema()
         // filter comments
-        val countOg = cit.count()
-        println(s"Original lines count: $countOg")
+        // val countOg = cit.count()
+        // println(s"Original lines count: $countOg")
         cit = cit.filter(!$"value".contains("#"))
-        val countFil = cit.count()
-        println(s"Filtered lines count: $countFil")
+        // val countFil = cit.count()
+        // println(s"Filtered lines count: $countFil")
         // create columns
         val citcleaned = cit.withColumn("fromnode", split(col("value"), "\t").getItem(0).cast("int"))
             .withColumn("tonode", split(col("value"), "\t").getItem(1).cast("int"))
@@ -26,7 +25,6 @@ object Citations1 {
 
         // Read published-dates from HDFS
         var pd = spark.read.textFile("hdfs:///pa1/published-dates.txt")
-        pd.printSchema()
         pd = pd.filter(!$"value".contains("#"))
         // create columns
         val pdcleaned = pd.withColumn("nodeid", split(col("value"), "\t").getItem(0).cast("int"))
@@ -36,17 +34,18 @@ object Citations1 {
         pdcleaned2.show()
         pdcleaned2.printSchema()
 
+        // Crreate views
+        citcleaned.createOrReplaceTempView("citations")
+        pdcleaned2.createOrReplaceTempView("pdates")
 
-
-        // cit.createOrReplaceTempView("citations")
-        // val query1 = """
-        //     SELECT *, SPLIT(value, '\t')[0] as fromnode, SPLIT(value, '\t')[1] as tonode 
-        //     FROM citations 
-        //     LIMIT 10
-        // """;
-        // cit = spark.sql(query1)
+        val query1 = """
+            SELECT *
+            FROM citations 
+            LIMIT 10
+        """;
+        val c1 = spark.sql(query1)
         // val citcleaned = spark.sql("SELECT *, SPLIT(value, '\t')[0] as fromnode, SPLIT(value, '\t')[1] as tonode FROM citations LIMIT 10")
-
+        c1.show()
 
 
 
