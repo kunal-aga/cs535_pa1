586856b6cb5c076490b5cfc5e46406b9a39ceb2a
Kunal Agarwal <65165791+iamkunalagarwal@users.noreply.github.com>
2023-02-15 13:24:45 -0700

    Update Citations2.scala

--- graph_diameter/src/main/scala/Citations2.scala
+++ graph_diameter/src/main/scala/Citations2.scala
@@ -31,7 +31,7 @@ object Citations2 {
         // Seq (array) to save stats per year
         var resultData: Seq[Row] = Seq.empty[Row]
 
-        for( year <- 1992 to 1993)
+        for( year <- 1992 to 2002)
         {
             println(s"********* Year : $year **************")
 
@@ -181,7 +181,7 @@ object Citations2 {
         val result = spark.createDataFrame(spark.sparkContext.parallelize(resultData), resultSchema)
         result.printSchema()
         result.show()
-        val outputPath = "hdfs:///pa1/graph_diameter_07"
+        val outputPath = "hdfs:///pa1/graph_diameter_full"
         result.coalesce(1).write.format("csv").save(outputPath)
 
         spark.stop()
